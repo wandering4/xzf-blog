69bfd2d507f146c450ecc4896f07ff993edd2f66
徐梓凡 <17891997260@139.com>
2026-01-30 00:34:47 +0800

    springboot3适配

--- blog-Framework/blog-spring-boot-starter-biz-context/src/main/java/com/xzf/framework/biz/context/filter/HeaderUserRole2ContextFilter.java
+++ blog-Framework/blog-spring-boot-starter-biz-context/src/main/java/com/xzf/framework/biz/context/filter/HeaderUserRole2ContextFilter.java
@@ -4,14 +4,14 @@ package com.xzf.framework.biz.context.filter;
 
 import com.xzf.blog.framework.commons.constant.GlobalConstants;
 import com.xzf.framework.biz.context.holder.LoginUserContextHolder;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
